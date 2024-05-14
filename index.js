377c208d5b55afd2cd4ea5980abda7a3ec492967
firoz.nur <firoznur5@gmail.com>
2024-05-14 16:43:13 +0600

    get req food in server

--- index.js
+++ index.js
@@ -45,6 +45,13 @@ async function run() {
       }
     });
 
+    app.get("/requestedFood/:email", async (req, res) => {
+      const result = await requestedFoodCollection
+        .find({ requestedUserEmail: req.params.email })
+        .toArray();
+      res.send(result);
+    });
+
     app.get("/food/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
